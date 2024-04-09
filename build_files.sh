d9e8ab9466f5bc90779dfde9730b65d124d8143e
Sanket-Ugale <sanketugale683@outlook.com>
2024-04-09 19:21:45 +0530

    Update build_files.sh to use pip instead of @vercel/python builder

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-# Use the Python and pip binaries provided by the @vercel/python builder
-PYTHON_BIN="/opt/python3.9/bin/python3.9"
-PIP_BIN="/opt/python3.9/bin/pip3.9"
-
-# Install Django and the requirements
-$PIP_BIN install django
-$PIP_BIN install -r requirements.txt
+pip install django
+pip install -r requirements.txt
