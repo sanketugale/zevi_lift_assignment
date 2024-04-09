a930fac06aed8396a9417763c92f9c5cb1148f53
Sanket-Ugale <sanketugale683@outlook.com>
2024-04-09 19:19:57 +0530

    Update build_files.sh to use @vercel/python builder and install Django and requirements

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-pip install django
-pip install -r requirements.txt
+#!/bin/bash
+
+# Use the Python and pip binaries provided by the @vercel/python builder
+PYTHON_BIN="/opt/python3.9/bin/python3.9"
+PIP_BIN="/opt/python3.9/bin/pip3.9"
+
+# Install Django and the requirements
+$PIP_BIN install django
+$PIP_BIN install -r requirements.txt
